a1a78b6b67c0934cf836c79c98bdc4900930b38c
jumentib <jumentib@eduroam-112098.grenet.fr>
2019-03-11 16:12:08 +0100

    ini

--- R/useFonc.r
+++ R/useFonc.r
@@ -642,6 +642,7 @@ gemma <- function(geno, pheno, map, covar = NULL, gemma.exe = "./gemma.macosx",
 #' @param pmin : for bslmm : initial no zero proportion of beta (pi)
 #' @param pmax : for bslmm : max no zero proportion of beta (pi)
 #' @param opt.bs : option for bslmm : w and s default as in Zeng 2017 (10000 for both)
+#' @param smax : Number of variants with sparse effects (~ number of major effect loci). Default to 300 like in gemma
 #' @return : a gemma result
 #'
 #' @details
@@ -652,7 +653,7 @@ gemma <- function(geno, pheno, map, covar = NULL, gemma.exe = "./gemma.macosx",
 gemma2 <- function(geno, pheno, map, covar = NULL, gemma.exe = "./gemma.macosx",
                    opt.mat = c(1,2), met.reg = c("lmm", "bslmm"),
                    lmm = c(1,2,3,4), bslmm = c(1,2,3), pmin = 0.01, pmax = 0.05,
-                   opt.bs = "-w  10000 -s  10000") {
+                   opt.bs = "-w  10000 -s  10000", smax = 300) {
 
   system("mkdir gemma")
 
@@ -682,7 +683,7 @@ gemma2 <- function(geno, pheno, map, covar = NULL, gemma.exe = "./gemma.macosx",
     pmax <- log10(pmax)
     system(paste(gemma.exe,"-g gemma/geno.txt -p gemma/pheno.txt",
                  " -a gemma/map.txt -k output/kin.cXX.txt -bslmm", bslmm," -o bslmm",
-                 opt.bs, "-pmin", pmin, "-pmax", pmax),
+                 opt.bs, "-pmin", pmin, "-pmax", pmax, "-smax", smax),
            ignore.stdout = F)
     res <- read.table("output/bslmm.param.txt", header = T)
   }
@@ -801,4 +802,290 @@ panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
   txt <- paste0(prefix, txt)
   if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
   text(0.5, 0.5, txt, cex = cex.cor * r)
+}
+
+
+#' f1.score.prs : Calcul of the F1 score and the Polygenic Risk Score (PRS) for sparse regression
+#'
+#' @param sparse.effect : effect from a sparse regression
+#' @param causal : true causal SNP
+#' @param genotype : genotype --> one row per SNP, and one column per sample.
+#' @param phenotype : phenotype of interest
+#'
+#' @return Return the F1 Score, the precision, the recall, the prediction of the phenotype and the r2 between the phenotype and x.pred.
+#'
+#' @export
+f1.score.prs <- function(sparse.effect, causal, genotype, phenotype) {
+
+  nozero.eff <- which(sparse.effect != 0)
+  zero.eff   <- which(sparse.effect == 0)
+  # F1 score
+  TP <- sum(nozero.eff %in% causal)
+  FP <- sum(!(nozero.eff %in% causal))
+  FN <- sum(causal %in% zero.eff)
+
+  preci <- TP / (TP + FP)
+  recal <- TP / (TP + FN)
+
+  F1 <- 2 * (preci * recal) / (preci + recal)
+
+  # PRS
+  x.pred <- genotype[, nozero.eff] %*% matrix(sparse.effect[nozero.eff])
+  r2 <- summary(lm(x.pred ~ phenotype))$r.squared
+  return(list(f1.score = F1, r2 = r2, precision = preci, recall = recal, x.pred = x.pred))
+}
+
+
+
+#' rank.pwer.sparse : Calcul the AUC of the Hits Rank Curve for sparse regresion
+#'
+#' @param sparse.effect : effect from a sparse regression
+#' @param causal : true causal SNP
+#'
+#' @return Return the AUC of the Hits Rank Curve
+#'
+#' @export
+rank.pwer.sparse <- function(sparse.effect, causal) {
+  # function
+  n.nozero <- sum(sparse.effect != 0)
+  selec <- order(abs(sparse.effect), decreasing = T)[1:n.nozero]
+  cur <- 0 # rank power curve
+
+  for (i in 1:n.nozero) {
+    cur <- c(cur,sum(selec[1:i] %in% causal))
+  }
+  auc <- sum(cur) # AUC of rank power curve
+
+  # calcul AUC max
+  nozero.eff <- which(sparse.effect != 0)
+  TP <- sum(nozero.eff %in% causal)
+  FP <- sum(!(nozero.eff %in% causal))
+
+  cur.max <- c(1:TP, rep(TP, FP))
+  auc.max <- sum(cur.max)
+
+  return(list(auc.norm = auc/auc.max, auc = auc, cur = cur))
+}
+
+
+#' cbind.na : cbind.na
+#'
+#' @export
+cbind.na <- function (..., deparse.level = 1)
+{
+  na <- nargs() - (!missing(deparse.level))
+  deparse.level <- as.integer(deparse.level)
+  stopifnot(0 <= deparse.level, deparse.level <= 2)
+  argl <- list(...)
+  while (na > 0 && is.null(argl[[na]])) {
+    argl <- argl[-na]
+    na <- na - 1
+  }
+  if (na == 0)
+    return(NULL)
+  if (na == 1) {
+    if (isS4(..1))
+      return(cbind2(..1))
+    else return(matrix(...))  ##.Internal(cbind(deparse.level, ...)))
+  }
+  if (deparse.level) {
+    symarg <- as.list(sys.call()[-1L])[1L:na]
+    Nms <- function(i) {
+      if (is.null(r <- names(symarg[i])) || r == "") {
+        if (is.symbol(r <- symarg[[i]]) || deparse.level ==
+            2)
+          deparse(r)
+      }
+      else r
+    }
+  }
+  ## deactivated, otherwise no fill in with two arguments
+  if (na == 0) {
+    r <- argl[[2]]
+    fix.na <- FALSE
+  }
+  else {
+    nrs <- unname(lapply(argl, nrow))
+    iV <- sapply(nrs, is.null)
+    fix.na <- identical(nrs[(na - 1):na], list(NULL, NULL))
+    ## deactivated, otherwise data will be recycled
+    #if (fix.na) {
+    #    nr <- max(if (all(iV)) sapply(argl, length) else unlist(nrs[!iV]))
+    #    argl[[na]] <- cbind(rep(argl[[na]], length.out = nr),
+    #        deparse.level = 0)
+    #}
+    if (deparse.level) {
+      if (fix.na)
+        fix.na <- !is.null(Nna <- Nms(na))
+      if (!is.null(nmi <- names(argl)))
+        iV <- iV & (nmi == "")
+      ii <- if (fix.na)
+        2:(na - 1)
+      else 2:na
+      if (any(iV[ii])) {
+        for (i in ii[iV[ii]]) if (!is.null(nmi <- Nms(i)))
+          names(argl)[i] <- nmi
+      }
+    }
+
+    ## filling with NA's to maximum occuring nrows
+    nRow <- as.numeric(sapply(argl, function(x) NROW(x)))
+    maxRow <- max(nRow, na.rm = TRUE)
+    argl <- lapply(argl, function(x)  if (is.null(nrow(x))) c(x, rep(NA, maxRow - length(x)))
+                   else rbind.na(x, matrix(, maxRow - nrow(x), ncol(x))))
+    r <- do.call(cbind, c(argl[-1L], list(deparse.level = deparse.level)))
+  }
+  d2 <- dim(r)
+  r <- cbind2(argl[[1]], r)
+  if (deparse.level == 0)
+    return(r)
+  ism1 <- !is.null(d1 <- dim(..1)) && length(d1) == 2L
+  ism2 <- !is.null(d2) && length(d2) == 2L && !fix.na
+  if (ism1 && ism2)
+    return(r)
+  Ncol <- function(x) {
+    d <- dim(x)
+    if (length(d) == 2L)
+      d[2L]
+    else as.integer(length(x) > 0L)
+  }
+  nn1 <- !is.null(N1 <- if ((l1 <- Ncol(..1)) && !ism1) Nms(1))
+  nn2 <- !is.null(N2 <- if (na == 2 && Ncol(..2) && !ism2) Nms(2))
+  if (nn1 || nn2 || fix.na) {
+    if (is.null(colnames(r)))
+      colnames(r) <- rep.int("", ncol(r))
+    setN <- function(i, nams) colnames(r)[i] <<- if (is.null(nams))
+      ""
+    else nams
+    if (nn1)
+      setN(1, N1)
+    if (nn2)
+      setN(1 + l1, N2)
+    if (fix.na)
+      setN(ncol(r), Nna)
+  }
+  r
+}
+
+
+#' rbind.na : rbind.na
+#'
+#' @export
+rbind.na <- function (..., deparse.level = 1)
+{
+  na <- nargs() - (!missing(deparse.level))
+  deparse.level <- as.integer(deparse.level)
+  stopifnot(0 <= deparse.level, deparse.level <= 2)
+  argl <- list(...)
+  while (na > 0 && is.null(argl[[na]])) {
+    argl <- argl[-na]
+    na <- na - 1
+  }
+  if (na == 0)
+    return(NULL)
+  if (na == 1) {
+    if (isS4(..1))
+      return(rbind2(..1))
+    else return(matrix(..., nrow = 1)) ##.Internal(rbind(deparse.level, ...)))
+  }
+  if (deparse.level) {
+    symarg <- as.list(sys.call()[-1L])[1L:na]
+    Nms <- function(i) {
+      if (is.null(r <- names(symarg[i])) || r == "") {
+        if (is.symbol(r <- symarg[[i]]) || deparse.level ==
+            2)
+          deparse(r)
+      }
+      else r
+    }
+  }
+
+  ## deactivated, otherwise no fill in with two arguments
+  if (na == 0) {
+    r <- argl[[2]]
+    fix.na <- FALSE
+  }
+  else {
+    nrs <- unname(lapply(argl, ncol))
+    iV <- sapply(nrs, is.null)
+    fix.na <- identical(nrs[(na - 1):na], list(NULL, NULL))
+    ## deactivated, otherwise data will be recycled
+    #if (fix.na) {
+    #    nr <- max(if (all(iV)) sapply(argl, length) else unlist(nrs[!iV]))
+    #    argl[[na]] <- rbind(rep(argl[[na]], length.out = nr),
+    #        deparse.level = 0)
+    #}
+    if (deparse.level) {
+      if (fix.na)
+        fix.na <- !is.null(Nna <- Nms(na))
+      if (!is.null(nmi <- names(argl)))
+        iV <- iV & (nmi == "")
+      ii <- if (fix.na)
+        2:(na - 1)
+      else 2:na
+      if (any(iV[ii])) {
+        for (i in ii[iV[ii]]) if (!is.null(nmi <- Nms(i)))
+          names(argl)[i] <- nmi
+      }
+    }
+
+    ## filling with NA's to maximum occuring ncols
+    nCol <- as.numeric(sapply(argl, function(x) if (is.null(ncol(x))) length(x)
+                              else ncol(x)))
+    maxCol <- max(nCol, na.rm = TRUE)
+    argl <- lapply(argl, function(x)  if (is.null(ncol(x))) c(x, rep(NA, maxCol - length(x)))
+                   else cbind(x, matrix(, nrow(x), maxCol - ncol(x))))
+
+    ## create a common name vector from the
+    ## column names of all 'argl' items
+    namesVEC <- rep(NA, maxCol)
+    for (i in 1:length(argl)) {
+      CN <- colnames(argl[[i]])
+      m <- !(CN %in% namesVEC)
+      namesVEC[m] <- CN[m]
+    }
+
+    ## make all column names from common 'namesVEC'
+    for (j in 1:length(argl)) {
+      if (!is.null(ncol(argl[[j]]))) colnames(argl[[j]]) <- namesVEC
+    }
+
+    r <- do.call(rbind, c(argl[-1L], list(deparse.level = deparse.level)))
+  }
+
+  d2 <- dim(r)
+
+  ## make all column names from common 'namesVEC'
+  colnames(r) <- colnames(argl[[1]])
+
+  r <- rbind2(argl[[1]], r)
+
+  if (deparse.level == 0)
+    return(r)
+  ism1 <- !is.null(d1 <- dim(..1)) && length(d1) == 2L
+  ism2 <- !is.null(d2) && length(d2) == 2L && !fix.na
+  if (ism1 && ism2)
+    return(r)
+  Nrow <- function(x) {
+    d <- dim(x)
+    if (length(d) == 2L)
+      d[1L]
+    else as.integer(length(x) > 0L)
+  }
+  nn1 <- !is.null(N1 <- if ((l1 <- Nrow(..1)) && !ism1) Nms(1))
+  nn2 <- !is.null(N2 <- if (na == 2 && Nrow(..2) && !ism2) Nms(2))
+  if (nn1 || nn2 || fix.na) {
+    if (is.null(rownames(r)))
+      rownames(r) <- rep.int("", nrow(r))
+    setN <- function(i, nams) rownames(r)[i] <<- if (is.null(nams))
+      ""
+    else nams
+    if (nn1)
+      setN(1, N1)
+    if (nn2)
+      setN(1 + l1, N2)
+    if (fix.na)
+      setN(nrow(r), Nna)
+  }
+  r
 }
